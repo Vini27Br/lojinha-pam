f3734b2519e85d2c6412383255bfa0f669d17a23
Vini27Br <viniciusouza0@gmail.com>
2023-08-31 08:54:24 -0300

    fazendo coisas no app.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
+
+
+
 import React from 'react';
 
 import { SafeAreaView,  StatusBar } from 'react-native';
+import Catalogo from './src/pages/catalogo/index';
 
 export default function App() {
+
+
   return (
     <SafeAreaView>
         <StatusBar/>
-    
+        <Catalogo/>
     </SafeAreaView>
   );
+
 }
--- src/pages/catalogo/index.tsx
+++ src/pages/catalogo/index.tsx
@@ -3,20 +3,66 @@ import {View, Dimensions, Image, StyleSheet, Text } from "react-native";
 
 import UnoTopo from "../../../assets/UnoTopo.jpg"
 
+import unoescada from "../../../assets/unoescada.jpg"
+
+import oitorodas from "../../../assets/8rodas.jpg"
+
 const width = Dimensions.get('screen').width;
 
 export default function Catalogo() {
 
     return <>
-    <Image source={ UnoTopo } />
+    <Image source={ UnoTopo } style={estilos.topo} />
     
+    <Text style={estilos.titulo}>ESCOLHA O SEU CARRO E APOSTE</Text>
 
+
+    <View>
+
+    <Text >ESCOLHA SEU CARRO</Text>
+
+    <Text style={estilos.corredortitulo} > Trovão </Text>
+    <Image source={ unoescada } style={estilos.corredor}/>
+
+    <Text style={estilos.corredortitulo} > Alavanche </Text>
+    <Image source={ oitorodas } style={estilos.corredor}/>
+
+    </View>
     </>
 
 
 }
 
 const estilos = StyleSheet.create({ 
+
+    corredor: {
+        width: 300,
+        height: 150
+
+    },
+    corredortitulo: {
+        fontSize: 18,
+        marginTop: 5
+
+    },
+
+
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        color: "black",
+        textAlign: "center",
+        fontSize: 24,
+        lineHeight: 26,
+        padding: 16,
+        
+    },
+
+
 
 
 
