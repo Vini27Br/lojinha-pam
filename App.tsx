458e0e9c4d94b22cc6123d86f5640cc9d058cd7c
Vini27Br <viniciusouza0@gmail.com>
2023-09-11 11:58:20 -0300

    sim

--- App.tsx
+++ App.tsx
@@ -1,33 +1,12 @@
-import { useFonts, 
-  Montserrat_400Regular,
-  Montserrat_400Regular_Italic,
-  Montserrat_700Bold 
-} from '@expo-google-fonts/montserrat';
-
 import React from 'react';
 
 import { SafeAreaView,  StatusBar } from 'react-native';
-import Carrinho from './src/pages/Carrinho/index';
-
+import Catalogo from './src/pages/catalogo';
 
 export default function App() {
-  const [fonteCarregada] = useFonts({
-    "MonteserratRegular" : Montserrat_400Regular,
-    "MontserratBold": Montserrat_700Bold,
-    "MontserratItalic": Montserrat_400Regular_Italic
-  })
-
-  if (!fonteCarregada){
-    return null;
-  }
-
   return (
     <SafeAreaView>
         <StatusBar/>
-      <Carrinho/>
-    
+    <Catalogo/>
     </SafeAreaView>
-  );
-}
-
-
+  )}
